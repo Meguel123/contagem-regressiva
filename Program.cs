3fba94570fe531d5d1526018193dc27a205c8cb4
Miguel P <miguel.perassoli@gmail.com>
2025-02-28 20:22:06 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string X;
+        Console.Clear();
+Console.WriteLine("escreva -vamos OU simbora- qunado for para continuar");
+        X = Console.ReadLine();
+
+if (X.ToUpper() == "VAMOS"){
+Console.WriteLine("preparar");
+Console.ReadLine();
+
+Console.WriteLine("apontar");
+Console.ReadLine();
+
+
+Console.WriteLine("kkk ultima vez");
+Console.ReadLine();
+
+
+Console.WriteLine("FOGOOOOOOOOOOOOOOOOOOOOOO");
+Console.ReadLine();
+}
+
+else {
+Console.WriteLine("DIGITOU ERRADO");
+
+}
+	}
+}
+
+
